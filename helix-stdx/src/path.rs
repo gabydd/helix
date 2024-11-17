be4ba5d04d0671c75cabba843b461a455052ddda
kirawi <67773714+kirawi@users.noreply.github.com>
2024-11-17 00:47:31 -0500

    fix tests

--- helix-stdx/src/path.rs
+++ helix-stdx/src/path.rs
@@ -224,7 +224,7 @@ fn path_from_bytes(slice: &[u8]) -> Result<PathBuf, Utf8Error> {
 
 fn is_sep_byte(b: u8) -> bool {
     if cfg!(windows) {
-        b == b'/' || b == b'\\'
+        b == b'/' || b == b'\\' || b == b':'
     } else {
         b == b'/'
     }
@@ -233,7 +233,7 @@ fn is_sep_byte(b: u8) -> bool {
 /// Replaces all path separators in a path with %
 pub fn escape_path(path: &Path) -> PathBuf {
     let s = path.as_os_str().to_os_string();
-    let mut bytes = os_str_as_bytes(&s);
+    let mut bytes = os_str_as_bytes(s);
     for b in bytes.iter_mut() {
         if is_sep_byte(*b) {
             *b = b'%';
